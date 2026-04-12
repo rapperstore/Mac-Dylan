af373685534f09198617e559feef1334cc0d9a67
Mac Dylan <macdylanforever@gmail.com>
2026-04-12 08:26:28 -0600

    create store.html, fix store route, seed ebook

--- routes/store.py
+++ routes/store.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, render_template
-from database import db
 from models import Product
 
 store_bp = Blueprint('store', __name__)
 
 @store_bp.route('/')
 def index():
-    products = Product.query.filter_by(is_active=True).order_by(Product.created_at.desc()).all()
-    return render_template('store.html', products=products)
+    digital = Product.query.filter_by(is_active=True, product_type='digital').order_by(Product.created_at.desc()).all()
+    merch   = Product.query.filter_by(is_active=True, product_type='merch').order_by(Product.created_at.desc()).all()
+    courses = Product.query.filter_by(is_active=True, product_type='course').order_by(Product.created_at.desc()).all()
+    return render_template('store.html', digital=digital, merch=merch, courses=courses)
 
 @store_bp.route('/api/products')
 def api_products():
     products = Product.query.filter_by(is_active=True).all()
-    return {'products': [p.to_dict() for p in products]}
+    return {'products': [{'id':p.id,'name':p.name,'price':p.price,'type':p.product_type} for p in products]}
